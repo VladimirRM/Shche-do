e332b7ce54e6bed39ebf4df538715457e70d83ea
Vladimir RM <shvdrummer@gmail.com>
2023-07-10 13:44:47 +0300

    Add onChange and value for input

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import { useState } from "react"
 
 const App = () => {
   const [todos,setTodos] = useState([])
+  const [inputValue,setInputValue] = useState('')
 
 
   const addTodo = ()=> {
     const todo = {
       text: inputValue,
-      
+
     }
+    setTodos({...prev => prev.text})
   }
   return (
     <div>
-      
+      <input type="text" onChange={(e)=>setInputValue(e.target.value)} 
+      value={inputValue}/>
     </div>
   )
 }
