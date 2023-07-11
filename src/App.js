5d39a61485189ea8003da60de8b4874f772f9304
Vladimir RM <shvdrummer@gmail.com>
2023-07-11 15:32:43 +0300

    Update code

--- src/App.js
+++ src/App.js
@@ -2,32 +2,66 @@ import React,{useState} from 'react'
 import './App.css'
 
 const App = () => {
-  const [todos,setTodos] = useState([])
-  const [inputValue,setInputValue] = useState('')
-  const [edit, setEdit] = useState(null)
-  const [editValue,setEditValue] = useState('')
+  const [todos,setTodos] =useState([])
+  const [inputValue,setInputValue] =useState('')
+  const [edit,setEdit] =useState(null)
+  const [editValue,setEditValue] =useState('')
 
   const addTodo = ()=>{
     const todo = {
       text: inputValue,
       id:Date.now(),
-      completed: false,
+      completed: false
     }
     setTodos([...todos,todo])
-    setInputValue('')
+  }
+  const removeTodo = (id)=>{
+    setTodos([...todos].filter(todo=> todo.id !==id))
+  }
+  const editTodo = (id,text)=>{
+    setEdit(id)
+    setEditValue(text)
+  }
+  const saveTodo = (id)=>{
+        const editTodo = [...todos].map(todo=>{
+          if(todo.id === id){
+            todo.text = editValue
+          }
+          return todo
+        })
+        setTodos(editTodo)
+        setEdit(null)
   }
   return (
     <div>
       <input type="text"
-      value={inputValue}
       onChange={(e)=>setInputValue(e.target.value)}
+      value={inputValue}
       />
       <button onClick={addTodo}>Add</button>
 
-      <div>{todos.map((todo)=>(
-        <div>{todo.text}</div>
-      ))}</div>
-      
+      <div>
+        {todos.map(todo=>(
+
+            <div key={todo.id}>
+            {edit === todo.id ?
+             <div>
+              <input type="text"
+              onChange={(e)=>setEditValue(e.target.value)}
+              value={editValue} />
+              <button onClick={()=>saveTodo(todo.id)}>Save</button>
+             </div>  :
+             <div>
+                 {todo.text}
+                 <button onClick={()=>removeTodo(todo.id)}>Delete</button>
+                 <button onClick={()=>editTodo(todo.id,todo.text)}>Edit</button>
+             </div>
+          }
+            
+            </div>
+
+        ))}
+      </div>
     </div>
   )
 }
