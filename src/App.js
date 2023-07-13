16ff1753422b9db9a846d23cf8df73bbae4459ac
Vladimir RM <shvdrummer@gmail.com>
2023-07-13 17:28:00 +0300

    Delete code

--- src/App.js
+++ src/App.js
@@ -1,83 +1,42 @@
-import React,{useState} from 'react'
+import React ,{useState}from 'react'
 import './App.css'
 
 const App = () => {
-  const [todos,setTodos] =useState([])
-  const [inputValue,setInputValue] =useState('')
-  const [edit,setEdit] =useState(null)
-  const [editValue,setEditValue] =useState('')
-  // const [toggleCompleted,setToggleCompleted] =useState(false)
+  const [todos,setTodos] = useState([])
+  const [inputValue,setInputValue] = useState('')
 
-const addTodo = ()=>{
-  const todo = {
-    text: inputValue,
-    id:Date.now(),
-    completed: false
-  }
-  setTodos([...todos,todo])
-  setInputValue('')
-}
-const removeTodo = (id)=>{
-  setTodos([...todos].filter((todo)=>todo.id !==id))
-}
-const editTodo = (id,text)=>{
-  setEdit(id)
-  setEditValue(text)
-}
 
-const saveTodo =(id)=>{
-  const editTodo = [...todos].map((todo)=>{
-         if(todo.id === id){
-          todo.text = inputValue
-         }
-         return todo
-  })
-  setTodos(editTodo)
-  setEdit(null)
-}
 
-const toggleTodo = (id)=>{
-  setTodos(todos.filter((todo)=>{
-    if(todo.id===id){
-      todo.completed = !todo.completed
+
+  const addTodo = ()=>{
+    const todo = {
+      text: inputValue,
+      id:Date.now(),
+      completed: false
     }
-    return todo
-  }))
-}
+    setTodos([...todos,todo])
+    setInputValue('')
+  }
+const toggleTodo =(todo,id)=>{
+  setTodos([
+   ...todos.map((todo)=>todo.id?{...todo,complete:!todo.complete}:{...todo})
+  ])
+ }
 
   return (
     <div>
-      <input type="text"
-      onChange={(e)=>setInputValue(e.target.value)}
+      <input type="text" 
       value={inputValue}
+      onChange={e=>setInputValue(e.target.value)}
       />
       <button onClick={addTodo}>Add</button>
-
-      <div>
-        {todos.map(todo=>(
-
-            <div key={todo.id}>
-            {edit === todo.id ?
-             <div>
-              <input type="text"
-              onChange={(e)=>setEditValue(e.target.value)}
-              value={editValue} />
-              <button onClick={()=>saveTodo(todo.id)}>Save</button>
-             </div>  :
-             <div style={{ textDecoration: !todo.completed ? 'line-through': 'null'}}>
-                 {todo.text}
-                 <button onClick={()=>removeTodo(todo.id)}>Delete</button>
-                 <button onClick={()=>editTodo(todo.id,todo.text)}>Edit</button>
-                 <input type="checkbox" 
-                  onChange={()=> toggleTodo(todo.id)}
-                 />
-             </div>
-          }
-            
-            </div>
-
-        ))}
-      </div>
+      {todos.map((todo)=>(
+        <div>
+          <div  key={todo.id}className={todo.complete ?'checkBox':''} onClick={()=>toggleTodo(todo.id)}>{todo.text}</div>
+        
+        </div>
+        
+      ))}
     </div>
   )
 }
--- src/App1.js
+++ src/App1.js
@@ -1,77 +1,103 @@
-import { useState } from "react"
+import React,{useState} from 'react'
 import './App.css'
 
-
 const App = () => {
-  const [todos,setTodos] = useState([])
-  const [inputValue,setInputValue] = useState('')
-  const [edit,setEdit] = useState(null)
-  const [editValue,setEditValue] = useState('')
-  const [completed,setCompleted] = useState(false)
-  
-  
+  const [todos,setTodos] =useState([])
+  const [inputValue,setInputValue] =useState('')
+  const [edit,setEdit] =useState(null)
+  const [editValue,setEditValue] =useState('')
+  const [stateToggle,setStateToggle] =useState(false)
 
+const addTodo = ()=>{
+  const todo = {
+    text: inputValue,
+    id:Date.now(),
+    completed: false
+  }
+  setTodos([...todos,todo])
+  setInputValue('')
+}
+const removeTodo = (id)=>{
+  setTodos([...todos].filter((todo)=>todo.id !==id))
+}
+const editTodo = (id,text)=>{
+  setEdit(id)
+  setEditValue(text)
+}
 
-  const addTodo = ()=> {
-    const todo = {
-      text: inputValue,
-      id:Date.now(),
+const saveTodo =(id)=>{
+  const editTodo = [...todos].map((todo)=>{
+         if(todo.id === id){
+          todo.text = inputValue
+         }
+         return todo
+  })
+  setTodos(editTodo)
+  setEdit(null)
+}
 
-    }
-    setTodos([...todos, todo ])
-    setInputValue('')
-  }
-  const removeTodo = (id)=>{
-    setTodos([...todos.filter((todo)=> todo.id !== id)])
-  }
-  const editTodo = (id,text)=>{
-       setEdit(id)
-       setEditValue(text)
-  }
-  const saveTodo = (id)=>{
-     let editTodo = [...todos].map((todo)=>{
-      if(todo.id == id){
-          todo.text = editValue
-      }
-        return todo
-    })
-    setTodos(editTodo)
-    setEdit(null)
-  }
+const toggleTodo = (todo,id)=>{
+if(todo.id === id){
+  [...todos].map((todo)=> {
+    setStateToggle(!stateToggle)
+return todo
+  })
+  setTodos(todos)
+}
+ 
+ 
+}
+
+// const toggleTodo = (id) => {
+//   setTodos(
+//     [...todos].map((todo) =>
+//       todo.id ? { todo, complete: !todo.complete } : { ...todo }
+//     )
+//   );
+// // };
+// const toggleTodo =(todo,id)=>{
+//   setTodos([
+//    ...todos.map((todo)=>todo.id?{...todo,complete:!todo.complete}:{...todo})
+//   ])
+//  }
+
   return (
     <div>
-      <input type="text" onChange={(e)=>setInputValue(e.target.value)} 
-      value={inputValue}/>
-      <button onClick={addTodo}>Add todo</button>
-   
+      <input type="text"
+      onChange={(e)=>setInputValue(e.target.value)}
+      value={inputValue}
+      />
+      <button onClick={addTodo}>Add</button>
 
-            <div>
-                 <div>{todos?.map((todo)=>(
-            <div key={todo.id}  >
-             {edit === todo.id ? 
-            <div>
+      <div>
+        {todos.map(todo=>(
+
+            <div key={todo.id}>
+            {edit === todo.id ?
+             <div>
               <input type="text"
-              value={editValue}
               onChange={(e)=>setEditValue(e.target.value)}
-              />
-              <button onClick={()=> saveTodo(todo.id)}>Save</button>
-            </div> :
-            <div >
-                     {todo.text}
-          <button onClick={()=>editTodo(todo.id,todo.text)}>Edit</button>
-          <button onClick={()=>removeTodo(todo.id)}>Delete</button>
-          <input  type="checkbox"  onClick={()=>setCompleted(!completed)} 
-             
-          />
-            </div>
-            }       
-       
-        </div>
+              value={editValue} />
+              <button onClick={()=>saveTodo(todo.id)}>Save</button>
+             </div>  :
 
-      ))}</div>
-            </div>
+             <div key={todo.id} >
+                <div  
+                 className={stateToggle ? "checkBox": 'null'}
+                 >{todo.text}    <input type="checkbox" 
+                 onClick={()=>toggleTodo(todo.id)}
+                 /></div> 
+                 <button onClick={()=>removeTodo(todo.id)}>Delete</button>
+                 <button onClick={()=>editTodo(todo.id,todo.text)}>Edit</button>
+               
+                 
+             </div>
+          }
             
+            </div>
 
+        ))}
+      </div>
     </div>
   )
 }
