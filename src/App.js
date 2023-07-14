2999a2e981b07401d3be4e5717cb83b40141df79
Vladimir RM <shvdrummer@gmail.com>
2023-07-14 15:13:16 +0300

    Create main markup and constans.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import React ,{useState} from 'react'
-import "./App.css"
+import React,{useState} from 'react'
+import './App.css'
 
 const App = () => {
+  const [todos,setTodos] = useState([])
+  const [inputValue,setInputValue] = useState('')
+  const [edit,setEdit] = useState(null)
+  const [editValue,setEditValue] = useState('')
 
 
-  const [todos,setTodos]= useState([])
-  const [inputValue,setInputValue]= useState('')
-  const [edit, setEdit]= useState(null)
-  const [editValue, setEditValue]= useState('')
-
-  const addTodo = ()=>{
-    const todo = {
-      text: inputValue,
-      id:Date.now(),
-      // completed: false,
-    }
+const addTodo = ()=> {
+  const todo = {
+    text: inputValue,
+    id:Date.now(),
+    // completed: false,
   }
+  setTodos([...todos,todo])
+  setInputValue('')
+}
+const removeTodo = (id)=> {
+     setTodos([...todos].filter(todo=>todo.id !==id))
+}
+const editTodo = (id,text)=>{
+  setEdit(id)
+  setEditValue(text)
+}
 
   return (
     <div>
@@ -23,11 +31,15 @@ const App = () => {
       value={inputValue}
       onChange={(e)=>setInputValue(e.target.value)}
       />
-      <button onClick={addTodo}>Add todo</button>
-      {todos.map((todo)=>(
-        <div>{todo.text}</div>
-      ))}
-      
+      <button onClick={addTodo}>ADD</button>
+      <div>
+        {todos.map((todo)=>(
+          <div key={todo.id}>{todo.text}
+          <button onClick={()=>removeTodo(todo.id)}>Delete</button>
+          <button onClick={()=>editTodo(todo.id,todo.text)}>Edit</button>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
