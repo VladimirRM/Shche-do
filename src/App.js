b78b373932b30fe538ea465c17548fc29478b857
Vladimir RM <shvdrummer@gmail.com>
2023-07-16 13:02:08 +0300

    Create main state.

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React,{useState} from 'react'
+import './App.css'
+
+const App = () => {
+  const [todos,setTodos]=useState ([])
+  const [input,setInput]=useState ('')
+  const [edit,setEdit]=useState (null)
+  const [editValue,setEditValue]=useState ('')
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
