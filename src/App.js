c46e0362e2e4ee399e3a8e99a50ee8a9d4ae62c3
Vladimir RM <shvdrummer@gmail.com>
2023-07-17 08:43:23 +0300

    Add main  state

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React,{useState} from 'react'
 import './App.css'
 
 const App = () => {
-  
+    const [todos, setTodos] = useState([])
+    const [input, setInput] = useState('')
+    const [edit, setEdit] = useState(null)
+    const [editValue, setEditValue] = useState('')
   return (
     <div>
       
