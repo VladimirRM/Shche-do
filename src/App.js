ac5df41e737f36c47bec97899d24f9f81ac609c2
Vladimir RM <shvdrummer@gmail.com>
2023-07-19 08:03:28 +0300

    Create main states.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import './App.css'
 
 const App = () => {
 
-  const 
+
+  const [todos,setTodos]= useState([])
+  const [input,setInput]= useState('')
+  const [edit,setEdit]= useState(null)
+  const [editValue,setEditValue]= useState('')
   return (
     <div>
       
