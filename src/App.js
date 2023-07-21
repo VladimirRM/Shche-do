3cfba61cf6c38141856c21a9c2d5d4ead5243bf8
Vladimir RM <shvdrummer@gmail.com>
2023-07-21 09:46:41 +0300

    Created main state.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import './App.css'
 const App = () => {
+  const [ todos,setTodos] = useState([])
+  const [ input,setInput] = useState('')
+  const [ edit,setEdit] = useState(null)
+  const [ editValue,setEditValue] = useState(null)
 
-  
+
   return (
     <div>
       
