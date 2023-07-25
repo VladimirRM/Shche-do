1cb35e109f0ab34d88992ca3ab752bbe0e644dc1
Vladimir RM <shvdrummer@gmail.com>
2023-07-25 08:37:13 +0300

    Created main state.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import React from 'react'
+import React,{useState} from 'react'
+import './App.css'
 
 const App = () => {
+  const [todos,setTodos] = useState([])
+  const [input,setInput] = useState('')
+  const [edit,setEdit] = useState(null)
+  const [editValue,setEditValue] = useState('')
   return (
     <div>
       
