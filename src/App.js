57b63b9b64b4c4eb1a65616e8f2179934f1bf013
Vladimir RM <shvdrummer@gmail.com>
2023-07-26 07:47:52 +0300

    Add func editTodo.

--- src/App.js
+++ src/App.js
@@ -21,6 +21,11 @@ const App = () => {
   const removeTodo = (id)=>{
     setTodos([...todos].filter((todo)=>todo.id !==id))
   }
+  const editTodo = (id,text)=>{
+    setEdit(id)
+    setEditValue(text)
+  }
+  
   return (
     <div>
       
